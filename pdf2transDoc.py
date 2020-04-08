d09cd0085d1f5642f07cc7cd4b768daaa52f2897
a.u.sathe <38658819+ausathe@users.noreply.github.com>
2020-04-08 09:59:56 +0200

    Update pdf2transDoc.py

--- pdf2transDoc.py
+++ pdf2transDoc.py
@@ -16,19 +16,6 @@ import autoit
 from glob import glob
 import pdfkit
 
-##############################################################################################################################################################################
-# Steps to use this script:
-# PREREQUISUTES:
-# 1.	Install python and pip
-# 2.	(FOR WINDOWS ONLY) Open wkhtmltopdf_initial.exe and complete the setup
-# 3.	Insert the path to the wkhtmltopdf.exe file in your C-drive in line 59 of this script
-# 4.	Install Chrome and change the default download location of Chrome to the folder containing this script and the other contents
-# USAGE:
-# 1.	Place the PDF you want to translate in the folder containing the script and rename it to 'pdffile.pdf'.
-# 2.	Run the script
-# 3.	Do not make any actions on the computer until script is complete (COMPLETED MESSAGE IS DISPLAYED IN THE CONSOLE)
-##############################################################################################################################################################################
-
 def split_pdf(filename, groupsof):
 	# Import your PDF file:
 	inputpdf = PdfFileReader(open(filename, "rb"))
